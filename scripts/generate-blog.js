8d270b3521e373772ad1562b05acd4e6ae5974e0
zaddyjohn-cyber <ogbonnajohne@gmail.com>
2026-07-09 16:29:05 -0500

    Wire GA4 G-FL454XS8R0 across all pages and blog generator

--- scripts/generate-blog.js
+++ scripts/generate-blog.js
@@ -60,9 +60,9 @@ function buildPage({ slug, title, metaDesc, keywordList, dateStr, bodyHtml, read
   "mainEntityOfPage":{"@type":"WebPage","@id":"https://freelanceleadshub.shop/blog/${slug}.html"}
 }
 </script>
-<!-- GA4 — replace G-XXXXXXXXXX -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
-<script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-XXXXXXXXXX');</script>
+<!-- GA4 — replace G-FL454XS8R0 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FL454XS8R0"></script>
+<script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-FL454XS8R0');</script>
 <!-- Meta Pixel — replace YOUR_PIXEL_ID -->
 <script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','YOUR_PIXEL_ID');fbq('track','PageView');</script>
 <link rel="preconnect" href="https://fonts.googleapis.com" />
